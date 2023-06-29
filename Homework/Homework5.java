cc8f47e924f1c17b09b26e6f0f28427481bf64dd
NikolayKartaev <kartaevgeek@gmail.com>
2023-06-29 08:20:02 +0300

    Homework5

--- Homework/Homework5.java
+++ Homework/Homework5.java
@@ -2,18 +2,9 @@
  * Реализуйте структуру телефонной книги с помощью HashMap.
 Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными телефонами,
 их необходимо считать, как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
-
-Пример:
-Иванов 1231234
-Иванов 3456345
-Иванов 5676585
-Петров 12345
-Петров 82332
- * 
- */
+*/
 
 package JAVA.Homework;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -23,45 +14,45 @@ public class Homework5 {
 
     public static void main(String[] args) {
 
-        Map<String, ArrayList<String>> telephones = new HashMap<>();
+        Map<String, ArrayList<Integer>> phones = new HashMap<>();
 
-        addNumber(telephones, "Иванов", "123456");
-        addNumber(telephones, "Иванов", "234567");
-        addNumber(telephones, "Иванов", "345678");
-        addNumber(telephones, "Петров", "456789");
-        addNumber(telephones, "Петров", "567890");
-        addNumber(telephones, "Петров", "678901");
-        addNumber(telephones, "Петров", "789012");
+        addNumber(phones, "Сидоров", 876543);
+        addNumber(phones, "Иванов", 123456);
+        addNumber(phones, "Иванов", 234567);
+        addNumber(phones, "Иванов", 345678);
+        addNumber(phones, "Сидоров", 765432);
+        addNumber(phones, "Петров", 456789);
+        addNumber(phones, "Петров", 567890);
+        addNumber(phones, "Сидоров", 654321);
+        addNumber(phones, "Сидоров", 543219);
+        addNumber(phones, "Петров", 678901);
+        addNumber(phones, "Петров", 789012);
+        addNumber(phones, "Сидоров", 987654);
 
-        printPhoneBook(telephones);
+        sortPhones(phones);
 
     }
 
-    public static void addNumber(Map<String, ArrayList<String>> hashmap, String surname, String number) {
+    public static void addNumber(Map<String, ArrayList<Integer>> hashmap, String surname, Integer number) {
         if (hashmap.containsKey(surname)) {
-            ArrayList<String> uniqueNumbers = hashmap.get(surname);
-            uniqueNumbers.add(number);
+            ArrayList<Integer> AddedNumbers = hashmap.get(surname);
+            AddedNumbers.add(number);
         } else {
-            ArrayList<String> newNumbers = new ArrayList<>();
+            ArrayList<Integer> newNumbers = new ArrayList<>();
             newNumbers.add(number);
             hashmap.put(surname, newNumbers);
         }
-
     }
 
-    public static void printPhoneBook(Map<String, ArrayList<String>> phoneBook) {
-    ArrayList<Map.Entry<String, ArrayList<String>>> entries = new ArrayList<>(phoneBook.entrySet());
+    public static void sortPhones(Map<String, ArrayList<Integer>> hashmap) {
+        ArrayList<Map.Entry<String, ArrayList<Integer>>> entries = new ArrayList<>(hashmap.entrySet());
+        Collections.sort(entries, (entry1, entry2) -> entry2.getValue().size() - entry1.getValue().size());
 
-    // Сортировка списка по убыванию числа телефонов
-    Collections.sort(entries, (entry1, entry2) -> entry2.getValue().size() -
-    entry1.getValue().size());
-
-    // Вывод отсортированных контактов
-    for (Map.Entry<String, ArrayList<String>> entry : entries) {
-    String name = entry.getKey();
-    ArrayList<String> phoneNumbers = entry.getValue();
-    System.out.println(name + " " + phoneNumbers);
-    }
+        for (Map.Entry<String, ArrayList<Integer>> entry : entries) {
+            String surname = entry.getKey();
+            ArrayList<Integer> numbers = entry.getValue();
+            System.out.println(surname + ": " + numbers);
+        }
     }
 
 }
