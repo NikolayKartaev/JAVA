d46a3d5b8870cd60c07c37498bf4a80af6445187
NikolayKartaev <kartaevgeek@gmail.com>
2023-07-09 07:33:48 +0300

    HW1 OOP

--- family_tree/Human.java
+++ family_tree/Human.java
@@ -1,20 +1,22 @@
 package JAVA.family_tree;
 
-import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /** */
 public class Human {
     private String name;
     private String surname;
-    private Date dateOfBirth ;
-    private Date dateOfDeath;
+    private LocalDate dateOfBirth ;
+    private LocalDate dateOfDeath;
     private Gender gender;
     private Human mother;
     private Human father;
     private ArrayList<Human> children;
+    private ArrayList<Human> parents;
 
-    public Human(String name, String surname, Date dateOfBirth, Date dateOfDeath, Human mother, Human father) {
+    public Human(String name, String surname, LocalDate dateOfBirth, LocalDate dateOfDeath, Human mother, Human father, Gender gender) {
         this.name = name;
         this.surname = surname;
         this.dateOfBirth = dateOfBirth;
@@ -22,22 +24,27 @@ public class Human {
         this.mother = mother;
         this.father = father;
         this.children = new ArrayList<>();
+        this.gender=gender;
 
     }
 
-    public Human(String name, String surname, Date dateOfBirth, Human mother, Human father) {
-        this(name, surname, dateOfBirth, null, mother, father);
+    public Human(String name, String surname, LocalDate dateOfBirth, Human mother, Human father, Gender gender) {
+        this(name, surname, dateOfBirth, null, mother, father, gender);
     }
 
-    public Human(String name, String surname, Date dateOfBirth) {
+    public Human(String name, String surname, LocalDate dateOfBirth, Gender gender) {
+        this(name, surname, dateOfBirth, null, null, gender);
+    }
+
+    public Human(String name, String surname, LocalDate dateOfBirth) {
         this(name, surname, dateOfBirth, null, null, null);
     }
 
-    public void setDateOfDeath(Date dateOfDeath) {
+    public void setDateOfDeath(LocalDate dateOfDeath) {
         this.dateOfDeath = dateOfDeath;
     }
 
-    public void setDateOfBirth(Date dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -49,14 +56,24 @@ public class Human {
         return children;
     }
 
-    public void addChild(Human child) {
+    public void addChildM(Human child) {
         child.mother = this;
+        
+        children.add(child);
+    }
+    public void addChildF(Human child) {
+        child.father = this;
         children.add(child);
     }
 
+
+
+    
+    
+
     @Override
     public String toString() {
-        return String.format("Имя, фамилия: %s %s\nДата рождения: %d\n", name, surname, dateOfBirth);
+        return String.format("Имя, фамилия: %s %s\nДата рождения: %s\n", name, surname, dateOfBirth.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
     }
 
     public Human getFather() {
@@ -67,12 +84,33 @@ public class Human {
         return mother;
     }
 
-    ArrayList<Human> parents;
-
+    
+    
     public ArrayList<Human> getParents() {
         parents = new ArrayList<>();
-        parents.add(this.mother);
-        parents.add(this.father);
+        if (mother != null) {
+            parents.add(mother);
+        }
+        if (father != null) {
+            parents.add(father);
+        }
         return parents;
     }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+    
 }
--- family_tree/Main.java
+++ family_tree/Main.java
@@ -1,29 +1,32 @@
 package JAVA.family_tree;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Human human1 = new Human("Nikolay", "Kartaev", January 1, 1970, 00:00:00 GMT);
-        Human human2 = new Human("Anastasia", "Kartaeva", 26081986);
-        Human human3 = new Human("Platon", "Kartaev", 22012022, -1, human2, human1);
+        Human human4 = new Human("Olga", "Serduk", LocalDate.of(1965, 3, 13));
+        Human human5 = new Human("Sergey", "Kartaev", LocalDate.of(1964, 6, 15));
 
+        Human human1 = new Human("Nikolay", "Kartaev", LocalDate.of(1987, 1, 3), human4, human5, Gender.Male);
+        Human human2 = new Human("Anastasia", "Kartaeva", LocalDate.of(1986, 8, 26), Gender.Female);
+        Human human3 = new Human("Platon", "Kartaev", LocalDate.of(2022, 1, 30), human2, human1, Gender.Male);
 
-        human1.addChild(human2);
+        human1.addChildF(human3);
 
-        ArrayList<Human> children = human2.getChildren();
-
-        System.out.println(human2);
-
+        ArrayList<Human> children = human1.getChildren();
         for (Human human : children) {
-        System.out.println(human);
+            System.out.println(human);
         }
 
         System.out.println(human3.getFather());
         System.out.println(human3.getMother());
+
+        System.out.println(human1.getParents());
         System.out.println(human3.getParents());
+
     }
 
 }
